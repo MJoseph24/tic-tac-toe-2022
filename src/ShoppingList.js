093416b91dc9bb898e59d32ae1a81d6dcf0f7580
Melvin <melvin.joejoseph@gmail.com>
2022-10-28 09:02:34 -0400

    made shopping list

---
+++ src/ShoppingList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+import "./index.css";
+
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping list for {this.props.name}</h1>
+        <ul>
+          <li>pancakes</li>
+          <li>preboiled eggs</li>
+          <li>peaches</li>
+          <li>diet coke</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default ShoppingList;
