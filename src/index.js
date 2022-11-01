2b8394e507ee005eb5a92203ac99f5d71f475c9f
Melvin <melvin.joejoseph@gmail.com>
2022-11-01 15:09:01 -0400

    added squares and next player

--- src/index.js
+++ src/index.js
@@ -3,5 +3,52 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import ShoppingList from "./ShoppingList";
 
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div>
+          <Board />
+        </div>
+      </div>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+  render() {
+    const status = "X";
+    return (
+      <div className="board">
+        <div>Next Player: {status}</div>
+        <div className="first-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="second-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="third-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Square extends React.Component {
+  render() {
+    return <button className="square">{/*todo*/}</button>;
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<ShoppingList name= "Melvin" item="nutella food service container"/>);
+root.render(<Game />);
